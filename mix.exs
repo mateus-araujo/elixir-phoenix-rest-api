2907fb9797d77a282674df8aed0918c8a2dee6e0
Mateus Araujo <mateus_1542@@hotmail.com>
2020-12-06 20:01:39 -0300

    feat(books-crud): create store books controller and routes

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule BooksApi.MixProject do
   def application do
     [
       mod: {BooksApi.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
